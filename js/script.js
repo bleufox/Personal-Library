8527f1d563e5f90d2def62a5347a921cba95c11a
Andy <83156697+crocodilecomposition@users.noreply.github.com>
2021-06-16 19:29:20 -0700

    Local storage function just about done

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 // --------------------------------------------------------------------------------
 // Book log function - log books in a personal library
 
+const libraryArr = [];
+
 // Code adapted from: https://stackoverflow.com/questions/52505323/save-input-value-to-local-storage-and-retrieve-it-on-a-different-page
 function getBook(){
+    console.log(localStorage.getItem("bookStorage"));
     return localStorage.getItem("bookStorage");
 };
 
@@ -10,7 +13,6 @@ function updateHTML(){
     const bookEl = getBook();
     document.getElementById("submitReturn").style = "Color: grey";
     document.getElementById("submitReturn").innerHTML = bookEl + " has been added!";
-    // document.getElementById("savedBook").innerHTML = bookEl;
 };
 
 function saveBook(){
@@ -20,14 +22,19 @@ function saveBook(){
     // Saves data to retrieve later
     localStorage.setItem("bookStorage", bookInputEl);
 
-    // Updates HTML
     updateHTML();
 
-    console.log(`In this array: ${bookInputEl}`);
+    libraryArr.push(bookInputEl);
+     
+    console.log(`In this array: ${libraryArr}`);
+
+    savedBooks()
 };
 
-// function savedBooks(){
-
-// };
+function savedBooks(){
+    const savedBookEl = document.getElementById("bookLibrary");    
+    const PostLibraryArr = libraryArr.join(`, `);
+    savedBookEl.textContent = `${PostLibraryArr}`;
+};
 
 // --------------------------------------------------------------------------------
