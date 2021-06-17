c33644ba61a5d2b3a6f6bbbe178747cc0f8d35b0
bleufox <thebleufox8@gmail.com>
2021-06-16 21:20:17 -0700

    BFUpload4

--- js/script.js
+++ js/script.js
@@ -12,22 +12,22 @@ document.body.append(descriptionEl);
 document.body.append(bookImgEl);
 
 // Searches the api using user's query and gives top 10 results
-function getAPI(bookSearch){
+function getAPI(bookSearch) {
     fetch(`https://www.googleapis.com/books/v1/volumes?q=${bookSearch}`)
-    .then(function (response){
-        return response.json()
-    })
-    .then(handleData);
+        .then(function (response) {
+            return response.json()
+        })
+        .then(handleData);
 }
 
 submitBtn.addEventListener('click', handleClick)
-function handleClick(){
+function handleClick() {
     const userQuery = searchInputEl.value;
     getAPI(userQuery);
 }
 
 // pulls data from 10 results based on desired parameters
-function handleData(data){
+function handleData(data) {
     const bookInfo = data.items;
     console.log(bookInfo)
     for (let i = 0; i < bookInfo.length; i++) {
@@ -37,7 +37,7 @@ function handleData(data){
         const description = bookInfo[i].volumeInfo.description;
         const bookImg = bookInfo[i].volumeInfo.imageLinks.thumbnail;
         // !!--appends book image to page BUT looks like it stacks all 10 images and/or defaults to last image in the array to append..EEK! might needs hlep figuring that one out
-        bookImgEl.setAttribute('src', bookImg) 
+        bookImgEl.setAttribute('src', bookImg)
         //--!!
     }
 }
@@ -48,21 +48,21 @@ function handleData(data){
 // Book log function - log books in a personal library
 
 // Code adapted from: https://stackoverflow.com/questions/52505323/save-input-value-to-local-storage-and-retrieve-it-on-a-different-page
-function getBook(){
+function getBook() {
     return localStorage.getItem("bookStorage");
 };
 
-function updateHTML(){
+function updateHTML() {
     const bookEl = getBook();
     document.getElementById("submitReturn").style = "Color: grey";
     document.getElementById("submitReturn").innerHTML = bookEl + " has been added!";
     // document.getElementById("savedBook").innerHTML = bookEl;
 };
 
-function saveBook(){
+function saveBook() {
     // Gets input value
     const bookInputEl = document.getElementById("bookInput").value;
-  
+
     // Saves data to retrieve later
     localStorage.setItem("bookStorage", bookInputEl);
 
