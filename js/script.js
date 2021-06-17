f0feed529ca01ab38ada091861d742fe15031681
Andy <83156697+crocodilecomposition@users.noreply.github.com>
2021-06-16 20:15:00 -0700

    Book log function more or less done, separated from search bar function

--- js/script.js
+++ js/script.js
@@ -37,45 +37,44 @@ function handleData(data){
         const description = bookInfo[i].volumeInfo.description;
         const bookImg = bookInfo[i].volumeInfo.imageLinks.thumbnail;
         // !!--appends book image to page BUT looks like it stacks all 10 images and/or defaults to last image in the array to append..EEK! might needs hlep figuring that one out
-        bookImgEl.setAttribute('src', bookImg) 
+        bookImgEl.setAttribute('src', bookImg)
         //--!!
     }
 }
 
-
-
 // --------------------------------------------------------------------------------
 // Book log function - log books in a personal library
 
+const libraryArr = [];
+
 // Code adapted from: https://stackoverflow.com/questions/52505323/save-input-value-to-local-storage-and-retrieve-it-on-a-different-page
 function getBook(){
+    console.log(localStorage.getItem("bookStorage"));
     return localStorage.getItem("bookStorage");
 };
 
 function updateHTML(){
     const bookEl = getBook();
     document.getElementById("submitReturn").style = "Color: grey";
-    document.getElementById("submitReturn").innerHTML = bookEl + " has been added!";
-    // document.getElementById("savedBook").innerHTML = bookEl;
+    document.getElementById("submitReturn").innerHTML = `${bookEl} has been added!`;
+    // Add fade out effect?
 };
 
 function saveBook(){
     // Gets input value
     const bookInputEl = document.getElementById("bookInput").value;
-  
     // Saves data to retrieve later
     localStorage.setItem("bookStorage", bookInputEl);
-
-    // Updates HTML
     updateHTML();
-
-    console.log(`In this array: ${bookInputEl}`);
+    libraryArr.push(bookInputEl);
+    console.log(`In this array: ${libraryArr}`);
+    savedBooks()
 };
-getBook();
-updateHTML();
-saveBook();
-// function savedBooks(){
 
-// };
+function savedBooks(){
+    const savedBookEl = document.getElementById("bookLibrary");    
+    const PostLibraryArr = libraryArr.join(`, `);
+    savedBookEl.textContent = `${PostLibraryArr}`;
+};
 
 // --------------------------------------------------------------------------------
