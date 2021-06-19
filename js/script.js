ca5e5f41ea0388d5dfdca2e5e86e1cecf579b824
Mariam Ngoye <mariamngoye@gmail.com>
2021-06-19 10:59:58 -0700

    -message css framework

--- js/script.js
+++ js/script.js
@@ -12,42 +12,93 @@ document.body.append(descriptionEl);
 document.body.append(bookImgEl);
 
 // Searches the api using user's query and gives top 10 results
-function getAPI(bookSearch){
+function getAPI(bookSearch) {
     fetch(`https://www.googleapis.com/books/v1/volumes?q=${bookSearch}`)
-    .then(function (response){
-        return response.json()
-    })
-    .then(handleData);
+        .then(function (response) {
+            return response.json()
+        })
+        .then(handleData);
 }
 
 submitBtn.addEventListener('click', handleClick)
-function handleClick(){
+function handleClick() {
     const userQuery = searchInputEl.value;
     getAPI(userQuery);
 }
 
-// pulls data from 10 results based on desired parameters
+
+// Handles the parameters of title/author/genre/description/bookimg
 function handleData(data){
-    const bookInfo = data.items;
-    console.log(bookInfo)
-    for (let i = 0; i < bookInfo.length; i++) {
-        const title = bookInfo[i].volumeInfo.title;
-        const authors = bookInfo[i].volumeInfo.authors[0];
-        const genre = bookInfo[i].volumeInfo.categories;
-        const description = bookInfo[i].volumeInfo.description;
-        const bookImg = bookInfo[i].volumeInfo.imageLinks.thumbnail;
-        // !!--appends book image to page BUT looks like it stacks all 10 images and/or defaults to last image in the array to append..EEK! might needs hlep figuring that one out
-        bookImgEl.setAttribute('src', bookImg) 
-        //--!!
+        const bookInfo = data.items;
+        console.log('data array is:', data)
+        for (let i = 0; i < bookInfo.length; i++) {
+            handleTitle(bookInfo[i]); 
+            handleAuthor(bookInfo[i]);
+            handleGenre(bookInfo[i]);
+            handleDescription(bookInfo[i]);
+            handleImg(bookInfo[i]);
+        }
+}
+
+function handleTitle(book){
+    const title = book.volumeInfo.title;
+    if (title === undefined){
+        titleEl.textContent = 'No title listed.'
+    } else {
+        titleEl.textContent = title;
+        console.log('title is:' + title);
     }
 }
 
+function handleAuthor(book){
+    const author = book.volumeInfo.authors;
+    if (author === undefined){
+        authorEl.textContent = 'No author listed.'
+    } else {
+        authorEl.textContent = author;
+        console.log('author is:' + author);
+    }
+}
 
+function handleGenre(book){
+    const genre = book.volumeInfo.categories;
+    if (genre === undefined){
+        genreEl.textContent = 'No genre listed.';
+        console.log('no genre')
+    } else{
+        genreEl.textContent = genre;
+        console.log('genre is:' + genre);
+    }
+}
+
+function handleDescription(book){
+    const description = book.volumeInfo.description;
+    if (description === undefined){
+        console.log('no description');
+        descriptionEl.textContent = 'No description listed.'
+    } else {
+        const descripSnippet = description.split('.');
+        descriptionEl.textContent = descripSnippet[0] + '.';
+        console.log('description is:' + description);
+    }
+}
+
+function handleImg(book){
+    const bookImg = book.volumeInfo.imageLinks.thumbnail;
+    if (bookImg === undefined){
+        bookImgEl.textContent = 'No image available.'
+        console.log('no image')
+    }else {
+        bookImgEl.setAttribute('src', bookImg);
+        console.log('image link is:' + bookImg);
+    }
+}
 
 // --------------------------------------------------------------------------------
 // Book log function - log books in a personal library
 
-// Code adapted from: https://stackoverflow.com/questions/52505323/save-input-value-to-local-storage-and-retrieve-it-on-a-different-page
+const libraryArr = [];
+
 function getBook(){
     return localStorage.getItem("bookStorage");
 };
@@ -55,27 +106,49 @@ function getBook(){
 function updateHTML(){
     const bookEl = getBook();
     document.getElementById("submitReturn").style = "Color: grey";
-    document.getElementById("submitReturn").innerHTML = bookEl + " has been added!";
-    // document.getElementById("savedBook").innerHTML = bookEl;
+    document.getElementById("submitReturn").innerHTML = `${bookEl} has been added!`;
+    // Add fade out
 };
 
 function saveBook(){
-    // Gets input value
     const bookInputEl = document.getElementById("bookInput").value;
-  
-    // Saves data to retrieve later
     localStorage.setItem("bookStorage", bookInputEl);
-
-    // Updates HTML
     updateHTML();
-
-    console.log(`In this array: ${bookInputEl}`);
+    libraryArr.push(bookInputEl);
+    // console.log(`In this array: ${libraryArr}`);
+    savedBooks();
+    
+    // function updateHTML(){
+    //     libraryArr.push(bookInputEl);
+    //     console.log(`In this array: ${libraryArr}`);
+    //     savedBooks()
+    // };
 };
-getBook();
-updateHTML();
-saveBook();
-// function savedBooks(){
 
-// };
+function savedBooks(){
+    const savedBookEl = document.getElementById("bookLibrary");
+    const postLibraryArr = libraryArr.join(`, `);
+    savedBookEl.textContent = `${postLibraryArr}`;
+};
+
+// --------------------------------------------------------------------------------
+
+const lentBooks = [];
+const dueDateRowEl = document.querySelector("#due");
+
+function dueDateReminder(){
+    const dueDateEl = document.querySelector("#dueDates");
+    
+    for (let i = 0; i < lentBooks.length; i++) {
+        // const element = array[i];
+        if(lentBooks.length <= 0){
+            dueDateEl.textContent = "Hooray! No upcoming due dates!";
+        }else{
+            dueDateEl.textContent = `${dueDateEl} is due on ${dueDateEl}`;
+        };        
+    };
+};
+
+// dueDateReminder();
 
 // --------------------------------------------------------------------------------
