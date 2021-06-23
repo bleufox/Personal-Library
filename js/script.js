8066c5c32745bc0c3282b7d4ae2c6d40103de04c
Mariam Ngoye <mariamngoye@gmail.com>
2021-06-22 18:56:20 -0700

    -m "new css carousel"

--- js/script.js
+++ js/script.js
@@ -113,8 +113,8 @@ function showSlides(n) {
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex - 1].style.display = "block"; // These are not calling anything
-    dots[slideIndex - 1].className += " active";
+  //  slides[slideIndex - 1].style.display = "block"; // These are not calling anything
+    //dots[slideIndex - 1].className += " active";
 };
 
 // --------------------------- Add to local storage ---------------------------
