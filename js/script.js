b8bf95476bf5a4a70bf97c4f5c71d26caab0fa03
Mariam Ngoye <mariamngoye@gmail.com>
2021-06-23 18:46:44 -0700

    images and dots

--- js/script.js
+++ js/script.js
@@ -166,7 +166,7 @@ function updateHTML(item){
 //     return totalLibrary;
 // };
 
-// ---------- Carousel -------------// 
+//---------- Carousel -------------// 
 
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -180,21 +180,21 @@ function currentSlide(n) {
 };
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-//   console.log(slides);
-  var dots = document.getElementsByClassName("dot");
-//   console.log(dots);
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-//   slides[slideIndex-1].style.display = "block"; // These are not calling anything
-//   dots[slideIndex-1].className += " active";
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    console.log(slides);
+    var dots = document.getElementsByClassName("dot");
+    console.log(dots);
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block"; // These are not calling anything
+    dots[slideIndex - 1].className += " active";
 };
 
 // ----------------------- Remove from local storage -----------------------
