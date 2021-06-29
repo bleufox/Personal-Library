a986b53cb151858b81ba2f6c18b25fbf2ce307ce
bleufox <thebleufox8@gmail.com>
2021-06-29 16:13:40 -0700

    Fixed reset button, removed commented out code

--- js/script.js
+++ js/script.js
@@ -65,36 +65,8 @@ function handleClick(){
 // -------------- Handles defined data parameters from API fetch, to be added to local storage --------------
 
 function handleData(data){
-    // if (condition) {
-    //     function removeAll(){
-    //         // const elem = document.getElementById('book-search-results');
-    //         // elem.parentNode.removeChild(elem);
-    //         document.getElementById('bookInput').value = '';
-    //         document.getElementById('book-search-results > tbody').innerHTML = ''; // This is clearing too much
-    //         // document.querySelector('#book-search-results > tbody').innerHTML = '';
-    //         // console.log(document.getElementById('submitReturn'));
-    //         document.getElementById('submitReturn').innerHTML = '';
-    //         libraryArr = clearArr;
-    //         bookRowArr = clearArr;
-    //     };
 
-    //     let bookResultsTable = document.querySelector('#book-search-results');
-    //     // console.log(bookResultsTable);
-    //     const bookInfo = data.items;
-    //     function buildRow(book){
-    //         const shortDescription = book.volumeInfo.description?.split('.')[0];
-    //         let trEl = document.createElement('tr');
-    //         trEl.classList.add('book-info-row');
-    //         buildTdWithInfo(book.volumeInfo.title, trEl); 
-    //         buildTdWithInfo(book.volumeInfo.authors, trEl);
-    //         buildTdWithInfo(book.volumeInfo.categories, trEl);
-    //         buildTdWithInfo(shortDescription, trEl);
-    //         // console.log(book.volumeInfo.imageLinks.thumbnail, trEl, true);
-    //         buildTdWithInfo(book.volumeInfo.imageLinks.thumbnail, trEl, true);
-    //         // console.log(bookResultsTable);
-    //         bookResultsTable.append(trEl);
-    //     };
-    let bookResultsTable = document.querySelector('#book-search-results');
+    let bookResultsTable = document.querySelector('#results');
     // console.log(bookResultsTable);
     const bookInfo = data.items;
     function buildRow(book){
@@ -157,16 +129,6 @@ function handleClickSelection(event){
     };
 }
 
-// ============= I don't think this is being called anywhere, lets remove it if we aren't using it? ================
-// function browseLibrary(){
-//     const searchVal = document.getElementById('bookInput').value;
-//     savedLibraryArr.push(searchVal);
-//     localStorage.setItem('searchStorage', savedLibraryArr);
-//     console.log(bookRowArr[0]);
-//     updateHTML(bookRowArr[0]);
-// };
-// =================================================================================================================
-
 // --------------- Saves bookInfo from handleSelection function to local storage ---------------
 
 function saveBook(bookInfo){
@@ -213,88 +175,17 @@ function addToLibrary(){
     buildPersonalLibrary();
 };
 
-// ============= Not sure if this is going to be used in the future? If not lets delete ===============================
-
-// function updateHTML(){
-//     const bookEl = bookRowArr[0];
-//     const submitReturnEl = document.getElementById('submitReturn');
-//     if(!bookEl){
-//         console.log("Ain't nothing here")
-//     }else{
-//     // // console.log(bookEl);
-//     // while (bookEl.firstChild) {
-//     //     bookEl.removeChild(bookEl.firstChild)
-//     // };
-//     // console.log('TEST')
-//     // document.getElementById('submitReturn').innerHTML = 'TEST';
-//     submitReturnEl.style = 'color: grey';
-//     submitReturnEl.innerHTML = `${bookEl} has been added!`;
-//     // if (submitReturnEl.className.indexOf('hide') !== -1){
-//     //   fadeIn(img);
-//     //   this.innerHTML = 'Fade Out';
-//     // }
-//     // else{
-//     //   fadeOut(img);
-//     //   this.innerHTML = 'Fade In';    
-//     };
-// };
-
-// =========================================================================================================
-
 function fadeOut(el){
     el.classList.add('hide');
     el.classList.remove('show');
 };
 
-// --------------- Remove from Local Storage ---------------
-
-const removeEl = document.getElementById('delete');
-
-// removeEl.addEventListener('click', removeBook());
-
-// function removeBook(){
-//     libraryArr.remove(bookInputEl);
-// };
-
-// --------------- Due date reminder ---------------
-
-const lentBooks = ['Leviathan', 'Candide', 'War & Peace'];
-
-const dueDateRowEl = document.querySelector('#resultsDue');
-
-function dueDateReminder(){
-    const dueDateEl = document.querySelector('#dueDates');
-    lentBooks.push(dueDateEl);
-    for (let i = 0; i < lentBooks.length; i++){
-        // const element = array[i];
-        if (lentBooks.length <= 0){
-            dueDateEl.textContent = 'No upcoming due dates!';
-        } else{
-            // array.forEach(item => console.log(item));
-            // lentBooks.forEach(item => console.log(item));
-
-            // lentBooks.forEach(function, dueDateRowEl);
-            // dueDateEl.textContent = `${dueDateEl} is due on ${dueDateEl}`;
-        };
-    };
-};
-
-window.onload = function (){
-    dueDateReminder();
-};
-
 // --------------- Clears Search Results ---------------
 
-// function removeAll(){
-//     // const elem = document.getElementById('book-search-results');
-//     // elem.parentNode.removeChild(elem);
-//     document.getElementById('bookInput').value = '';
-//     document.getElementById('book-search-results > tbody').innerHTML = ''; // This is clearing too much
-//     // document.querySelector('#book-search-results > tbody').innerHTML = '';
-//     // console.log(document.getElementById('submitReturn'));
-//     document.getElementById('submitReturn').innerHTML = '';
-//     libraryArr = clearArr;
-//     bookRowArr = clearArr;
-// };
-
-
+function removeAll(){
+    document.getElementById("results").innerHTML = "";
+    // document.querySelector('#book-search-results > tbody').innerHTML = "";
+    document.getElementById("submitReturn").innerHTML = "";
+    libraryArr = clearArr
+    bookRowArr = clearArr
+};
